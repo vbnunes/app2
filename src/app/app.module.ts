182ead777602932aa1e91dee8ca5901ef5434191
Vinicius Bazzani Nunes <vnunes@daitanlabs.com>
2017-11-01 09:20:17 -0200

    including assets to the project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { TopoComponent } from './topo/topo.component';
+import { RodapeComponent } from './rodape/rodape.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    TopoComponent,
+    RodapeComponent
   ],
   imports: [
     BrowserModule
